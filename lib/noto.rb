de354bb0e021b0052166cd32212be9294630d2da
Ashish Garg <ashish.garg@headerlabs.com>
2017-09-20 22:38:27 +0530

    Test commit

--- lib/noto.rb
+++ lib/noto.rb
@@ -3,20 +3,24 @@ require "noto/version"
 module Noto
   module ClassMethods
 
-    # ---------------- Accessors ---------------- #
-
-
     # ---------------- Available Methods -------- #
     def noto(option = {})
-      logger.info "************************************"
-      logger.info option[:actions]
-      logger.info option[:notify]
-      logger.info "************************************"
+
     end
   end
 
   def self.included(base)
-    base.extend(ClassMethods)
+    base.class_eval do
+      extend ClassMethods
+      after_create :create_notification
+
+      private
+      def create_notification
+        logger.info "sasdasdasdasdasdasdasda"
+        logger.info "sasdasdasdasdasdasdasda"
+        logger.info "sasdasdasdasdasdasdasda"
+      end
+    end
   end
 end
 
