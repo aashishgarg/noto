20c2c62919afe4e843a42c4d3d305d7a3dbe2ef9
Ashish Garg <ashish.garg@headerlabs.com>
2017-09-20 23:06:17 +0530

    Test commit

--- lib/noto.rb
+++ lib/noto.rb
@@ -1,25 +1,29 @@
 require "noto/version"
 
 module Noto
-  module ClassMethods
+  module Core
 
     attr_accessor :options
     # ---------------- Available Methods -------- #
+    def initialize(params)
+      send "#{options}=", {}
+    end
+
     def noto(option = {})
-      @options.merge(options)
+      logger.info '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'
+      logger.info options
+      logger.info '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'
     end
   end
 
   def self.included(base)
     base.class_eval do
-      extend ClassMethods
+      extend Core
       after_create :create_notification
 
       private
       def create_notification
-        logger.info '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'
-        logger.info
-        logger.info '%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%'
+
       end
     end
   end
