ca51796e8af12570b7f3110ffd59c46aefa2f49d
Ashish Garg <ashish.garg@headerlabs.com>
2017-09-21 09:35:18 +0530

    Test commit

--- lib/noto.rb
+++ lib/noto.rb
@@ -2,27 +2,22 @@ require "noto/version"
 
 module Noto
   module Core
-
     # ---------------- Available Methods -------- #
     def noto(options = {})
-      @aa = 'asdas'
+      logger.info '&&&&&&&&&&&&&&&&&&&&&&&&&&&'
+      logger.info options
+      logger.info '&&&&&&&&&&&&&&&&&&&&&&&&&&&'
     end
   end
 
   def self.included(base)
     base.class_eval do
-
-      attr_accessor :aa
-
       extend Core
-
       after_create :create_notification
 
-      # private
+      private
       def create_notification
-        logger.info "***********************"
-        logger.info self.aa
-        logger.info "***********************"
+
       end
     end
   end
