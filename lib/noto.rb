25274b1b07e7b28875c16ad734bb16bf9e57e949
Ashish Garg <ashish.garg@headerlabs.com>
2017-10-04 19:47:24 +0530

    Added generator file.

--- lib/noto.rb
+++ lib/noto.rb
@@ -3,20 +3,17 @@ require "noto/version"
 module Noto
   module Core
     # ---------------- Available Methods -------- #
-    def noto(options = {})
-
+    def noto
+      puts "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&"
+      # puts options
+      puts "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&"
     end
   end
 
   def self.included(base)
     base.class_eval do
       extend Core
-      after_create :create_notification
 
-      private
-      def create_notification
-
-      end
     end
   end
 end
