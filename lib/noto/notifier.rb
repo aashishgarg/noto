ed281258e484132b83477359bd732c8bf5b11432
Ashish Garg <ashish.garg@headerlabs.com>
2017-10-04 22:25:13 +0530

    Module restructured.

--- lib/noto/notifier.rb
+++ lib/noto/notifier.rb
@@ -12,7 +12,7 @@ module Noto
     def valid_target?
       if @@target.is_a?(Hash)
         puts "*****************************"
-        puts self
+        puts self.constantize
         puts self.reflect_on_all_associations.collect(&:name)
         puts @@target.keys.first
         puts self.reflect_on_all_associations.collect(&:name).include?(@@target.keys.first)
