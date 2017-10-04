2766e255c17b30726e882c7749f633bf21a79ba8
Ashish Garg <ashish.garg@headerlabs.com>
2017-10-04 20:19:43 +0530

    Module restructured.

--- lib/noto/notifier.rb
+++ lib/noto/notifier.rb
@@ -1,10 +1,19 @@
 module Noto
   module Core
-    # ---------------- Available Methods -------- #
     def notifier(options = {})
+      invalid_keys = options.keys - valid_options
+
       puts '&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&'
-      puts options.keys
+      puts invalid_keys
       puts '&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&'
+    end
+
+    def valid_options
+      [:target, :notify_on, :message]
+    end
+
+    def valid_keys?
+
     end
   end
 end
