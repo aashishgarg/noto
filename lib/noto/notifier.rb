9edcde4b2782040d343be18a12c24bb23bbdd5af
Ashish Garg <ashish.garg@headerlabs.com>
2017-10-05 10:29:22 +0530

    Module restructured.

--- lib/noto/notifier.rb
+++ lib/noto/notifier.rb
@@ -13,6 +13,12 @@ module Noto
 
   def self.included(base)
     base.extend Core
+    puts "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+    puts self.inspect
+    puts "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+    puts base.inspect
+    puts "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+    puts "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
     base.notify_on.each do |action|
       base.send("after_#{action}_commit", :notify)
     end
