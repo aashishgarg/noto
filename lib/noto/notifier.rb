6e8d1b060f7501a14134b7002915a215090075e4
Ashish Garg <ashish.garg@headerlabs.com>
2017-10-05 08:40:50 +0530

    Module restructured.

--- lib/noto/notifier.rb
+++ lib/noto/notifier.rb
@@ -5,10 +5,7 @@ module Noto
       @@notify_on ||= options[:notify_on]
       @@message ||= options[:message]
 
-      # When target is a HASH
-      puts '************************************'
-      puts self.send("#{@@target.keys.first}").send("#{@@target.values.first}")
-      puts '************************************'
+
     end
   end
 
@@ -18,6 +15,15 @@ module Noto
       @@notify_on = []
       @@message = ''
       extend Core
+
+      after_commit :notify
+    end
+
+    private
+    def notify
+      puts "*****************************"
+      puts @@target
+      puts "*****************************"
     end
   end
 end
